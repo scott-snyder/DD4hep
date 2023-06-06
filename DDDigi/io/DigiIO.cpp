f5407d830f1d2d13d0900c2054e29ae02d938218
scott snyder <sss@karma>
2023-06-06 17:33:58 -0400

    Suppress deprecation warnings from podio.

--- DDDigi/io/DigiIO.cpp
+++ DDDigi/io/DigiIO.cpp
@@ -1,3 +1,4 @@
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 //==========================================================================
 //  AIDA Detector description implementation 
 //--------------------------------------------------------------------------
